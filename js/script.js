ddaf7d68a5588bbf075a282e2831fea4faf2bfd9
nguyenvorindo-lab <nguyenvorindo@gmail.com>
2026-06-23 13:48:25 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 const siteName = "Basketball Life";
 let topic = "Bóng Rổ - Lối Sống Và Đam Mê";
-let imageCount = 4; // Gồm 1 ảnh hero ẩn và 3 ảnh trong thư viện
+let imageCount = 4; 
 let isReady = true;
 
 console.log("Website:", siteName);
@@ -9,3 +9,10 @@ console.log("Chủ đề:", topic);
 console.log("Số ảnh:", imageCount);
 console.log("Đã sẵn sàng dùng JS?:", isReady);
 console.log("Kiểu dữ liệu topic:", typeof topic);
+
+const mainTitle = document.getElementById("main-title");
+
+
+if (mainTitle) {
+  mainTitle.textContent = "Bóng Rổ - Đam Mê Bứt Phá Mỗi Ngày!";
+}
