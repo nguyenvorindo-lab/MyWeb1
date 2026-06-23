c259928b6e3e6be3b76e9527478381d4b0d92408
nguyenvorindo-lab <nguyenvorindo@gmail.com>
2026-06-23 13:57:24 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 const siteName = "Basketball Life";
 let topic = "Bóng Rổ - Lối Sống Và Đam Mê";
 let imageCount = 4;
@@ -9,7 +8,6 @@ console.log("Chủ đề:", topic);
 console.log("Số ảnh:", imageCount);
 console.log("Đã sẵn sàng dùng JS?:", isReady);
 console.log("Kiểu dữ liệu topic:", typeof topic);
-
 
 const mainTitle = document.getElementById("main-title");
 
@@ -23,5 +21,14 @@ const helloResult = document.getElementById("helloResult");
 if (helloBtn && helloResult) {
   helloBtn.addEventListener("click", function () {
     helloResult.textContent = "Cảm ơn bạn đã ghé thăm MyWeb của Nguyễn Võ Rin Đô!";
+  });
+}
+
+const toggleAboutBtn = document.getElementById("toggleAboutBtn");
+const aboutContent = document.getElementById("aboutContent");
+
+if (toggleAboutBtn && aboutContent) {
+  toggleAboutBtn.addEventListener("click", function () {
+    aboutContent.classList.toggle("hidden");
   });
 }
